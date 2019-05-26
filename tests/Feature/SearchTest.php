6cce4f008f5569d3dae5334d7c129759182003d7
Povilas Korop <povilask007@gmail.com>
2019-05-26 11:34:27 +0300

    Search test

---
+++ tests/Feature/SearchTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Account;
+use App\AccountTransaction;
+use App\User;
+use Tests\TestCase;
+
+class SearchTest extends TestCase
+{
+
+    /**
+     * @group shouldRun
+     */
+    public function test_search_returns_correct_results()
+    {
+        $account = Account::create([
+            'code' => 'account-123456',
+            'name' => 'account-123456-name'
+        ]);
+
+        AccountTransaction::create([
+            'account_id' => $account->id,
+            'transaction_date' => now()->format('m/d/Y'),
+            'reference' => 'reference-123',
+            'journal' => 'journal-123',
+            'code' => 'transaction-123456',
+            'debit_amount' => 12.34,
+        ]);
+
+        $user = User::find(1);
+
+        // Searching for "123" query
+        $response = $this->actingAs($user)
+            ->get('/admin/search?search%5Bterm%5D=123&search%5B_type%5D=query');
+        $results = json_decode($response->content())->results;
+
+        // Test that we've found both account and transaction by "123" query
+        $this->assertEquals(2, count($results));
+
+        $this->assertEquals('account-123456-name', $results[0]->name);
+
+        $this->assertEquals('reference-123', $results[1]->reference);
+        $this->assertEquals('journal-123', $results[1]->journal);
+    }
+
+}
