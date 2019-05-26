19b39e705e7ab921158509a355f06c59491ee2a9
Povilas Korop <povilask007@gmail.com>
2019-05-26 11:37:15 +0300

    Search test - test for empty results

--- tests/Feature/SearchTest.php
+++ tests/Feature/SearchTest.php
@@ -45,4 +45,34 @@ class SearchTest extends TestCase
         $this->assertEquals('journal-123', $results[1]->journal);
     }
 
+    /**
+     * @group shouldRun
+     */
+    public function test_search_returns_empty_results()
+    {
+        $account = Account::create([
+            'code' => 'account-123456',
+            'name' => 'account-123456-name'
+        ]);
+
+        AccountTransaction::create([
+            'account_id' => $account->id,
+            'transaction_date' => now()->format('m/d/Y'),
+            'reference' => 'reference-123',
+            'journal' => 'journal-123',
+            'code' => 'transaction-123456',
+            'debit_amount' => 12.34,
+        ]);
+
+        $user = User::find(1);
+
+        // Searching for "789" query
+        $response = $this->actingAs($user)
+            ->get('/admin/search?search%5Bterm%5D=789&search%5B_type%5D=query');
+        $results = json_decode($response->content())->results;
+
+        // Test that there are no records by "789" query
+        $this->assertEquals(0, count($results));
+    }
+
 }
