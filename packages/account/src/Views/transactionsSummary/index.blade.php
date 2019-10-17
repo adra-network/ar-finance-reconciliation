54d66ff46a7cd95fe5bb582ae71a707fd4967896
Karolis <narkevicius.karolis@gmail.com>
2019-10-17 13:40:00 +0300

    move outstanding balance to account summary + move import selector to account detail tab

--- packages/account/src/Views/transactionsSummary/index.blade.php
+++ packages/account/src/Views/transactionsSummary/index.blade.php
@@ -9,33 +9,27 @@
         <div class="col">
             <div id="tabs">
                 <ul>
-                    <li><a href="#tabs-1">Account summary</a></li>
-                    <li><a href="#tabs-2">Account detail</a></li>
+                    <li><a href="#tab1">Account summary</a></li>
+                    <li><a href="#tab2">Account detail</a></li>
                 </ul>
-                <div id="tabs-1">
-                    @include('account::transactionsSummary.table1')
+                <div id="tab1">
+                    @if(isset($batchTable))
+                        @include('account::transactions.batchTable', ['disableButtons' => true])
+                    @endif
                 </div>
-                <div id="tabs-2">
+                <div id="tab2">
+                    <select name="import_select" id="import_select" class="check-after-change form-control form-control-sm mt-3 mb-3">
+                        <option value="">-- Choose import --</option>
+                        @foreach ($accountImports as $import)
+                            <option value="{{ $import->id }}" {{ (int)$selectedImport === $import->id ? 'selected' : '' }}>{{ $import->title }}</option>
+                        @endforeach
+                    </select>
+                    @include('account::transactionsSummary.table1')
+
+                    <div class="mt-3"></div>
                     @include('account::transactionsSummary.table2')
                 </div>
             </div>
-        </div>
-        <div class="col">
-            @if(isset($batchTable))
-                <div class="card">
-                    <div class="card-header">
-                        Outstanding balance
-                    </div>
-
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col">
-                                @include('account::transactions.batchTable', ['disableButtons' => true])
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            @endif
         </div>
     </div>
 
@@ -45,19 +39,34 @@
 @endsection
 
 @section('scripts')
-
+    @parent
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
     <script>
-      $(".check-after-change").change(function () {
-        let account_value = $("#account_select").val();
-        let import_value = $("#import_select").val();
-
-        if (account_value != 0 && import_value != "")
-          window.location = "{{ route('account.transactions.summary') }}?account_id=" + account_value + "&import=" + import_value;
-      });
-
       $(document).ready(function () {
         $("#tabs").tabs();
+      })
+    </script>
+    <script>
+      $(document).ready(function () {
+        $(".check-after-change").change(function () {
+          let account_value = $("#account_select").val();
+          let import_value = $("#import_select").val();
+
+          let query = '';
+          if (account_value != 0) {
+            query += 'account_id=' + account_value
+          }
+          if (import_value != '') {
+            if (account_value) {
+              query += '&'
+            }
+            query += 'import=' + import_value + '#tab2'
+          }
+
+          if (query != '') {
+            window.location = "{{ route('account.transactions.summary') }}?" + query
+          }
+        });
       })
     </script>
 @endsection
