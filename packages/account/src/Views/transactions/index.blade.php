c4f80e739d9498ce75b89c24c94d1bd1572a1aef
Karolis <narkevicius.karolis@gmail.com>
2019-11-08 09:30:50 +0200

    remove hash from url after search

--- packages/account/src/Views/transactions/index.blade.php
+++ packages/account/src/Views/transactions/index.blade.php
@@ -67,9 +67,19 @@
           $('html, body').animate({
             scrollTop: scrollTop - 100
           }, 1000);
+          remove_hash_from_url()
         }
 
       })
+
+      function remove_hash_from_url()
+      {
+        var uri = window.location.toString();
+        if (uri.indexOf("#") > 0) {
+          var clean_uri = uri.substring(0, uri.indexOf("#"));
+          window.history.replaceState({}, document.title, clean_uri);
+        }
+      }
     </script>
 
     <script>
