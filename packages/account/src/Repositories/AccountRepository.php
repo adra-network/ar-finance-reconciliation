e3038b6fe626cbf085310c41de2e116baee2d51f
Povilas Korop <povilask007@gmail.com>
2020-01-24 13:45:20 +0200

    Unsynced summaries - show more data

--- packages/account/src/Repositories/AccountRepository.php
+++ packages/account/src/Repositories/AccountRepository.php
@@ -4,6 +4,7 @@ namespace Account\Repositories;
 
 use Account\Models\AccountImport;
 use Account\Models\MonthlySummary;
+use Account\Services\SummaryBeginningBalanceChecker;
 use Illuminate\Support\Collection;
 
 class AccountRepository
@@ -18,9 +19,15 @@ class AccountRepository
             return collect();
         }
 
-        return MonthlySummary::with('account.user')
+        $summaries = MonthlySummary::with('account.user')
             ->where('beginning_balance_in_sync', false)
             ->where('account_import_id', $lastImport->id)
             ->get();
+
+        foreach ($summaries as $summary) {
+            $summary->checker = new SummaryBeginningBalanceChecker($summary);
+        }
+
+        return $summaries;
     }
 }
--- packages/account/src/Views/partials/unsyncedSummaries.blade.php
+++ packages/account/src/Views/partials/unsyncedSummaries.blade.php
@@ -4,7 +4,10 @@
             <h4>Summaries out of sync after last import</h4>
             <div class="alert alert-warning">
                 @foreach($summaries as $summary)
-                    - {{ $summary->account->name }}<br />
+                    - {{ $summary->account->name }}
+                    (ending balance: ${{ number_format($summary->checker->currentBalance / 100, 2) }},
+                    new opening balance: ${{ number_format($summary->checker->beginningBalance / 100, 2) }},
+                    difference ${{ number_format($summary->checker->diff() / 100, 2) }})<br />
                 @endforeach
                 <a href="{{ route('account.reports.summaries-out-of-sync') }}">View full report</a>
             </div>
