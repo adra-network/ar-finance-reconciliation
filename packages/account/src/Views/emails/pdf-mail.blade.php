f425e7cdfeb2d650712e8da17b07d237d1b1f901
Mark <mark.leroux@adra.org>
2020-05-15 08:47:32 -0400

    HTML Email: formatting
    Another few tweaks.

--- packages/account/src/Views/emails/pdf-mail.blade.php
+++ packages/account/src/Views/emails/pdf-mail.blade.php
@@ -1,9 +1,10 @@
 <div>Please find attached your Account Receivable balance as of {{ $statementDate }}.</div>
-<div><b>NOTE:</b> this is not a spam or phishing email, If you have and concerns please reach out to Richard O'Ffill or Nataly Alfaro.</div>
+<div><b>NOTE:</b> this is not a spam or phishing email. If you have any concerns please reach out to Richard O'Ffill or Nataly Alfaro.</div>
 <br>
 <br>
 <div>
 Kind regards,
+<br>
 Finance team
 </div>
 <div style="margin-top:5px;">
