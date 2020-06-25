79600a252d2b9598ab7774d4382dadddf5959f71
Mark <mark.leroux@adra.org>
2020-06-24 20:50:12 -0400

    Email formatting
    Added a few line spaces to make it easier to read.

--- packages/account/src/Views/emails/pdf-mail.blade.php
+++ packages/account/src/Views/emails/pdf-mail.blade.php
@@ -1,17 +1,18 @@
 
+<div>
 @isset($accountUserName)
 	Dear {{ $accountUserName }},
 @else 
 	Hi,
 @endisset
+</div>
 <br>
 <div>Please find attached your Account Receivable balance as of {{ $statementDate }}.</div>
-<div><b>NOTE:</b> This is not a spam or phishing email. If you have any concerns please reach out to Richard O'Ffill or Nataly Alfaro.</div>
 <br>
+<div><b>NOTE:</b> This is not a spam or phishing email. If you have any concerns please reach out to Richard O'Ffill or Nataly Alfaro.</div>
 <br>
 <div>
 Kind regards,
-<br>
 Nataly Alfaro
 </div>
 <div style="margin-top:5px;">
