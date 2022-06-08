72c1dc50bb3427352ba5adf1b1b15ad74ffdf0d3
adra-network <50133721+adra-network@users.noreply.github.com>
2022-06-09 05:33:05 +1000

    Update pdf-mail.blade.php
    Round two of the email wording changes based on feedback from Richard.

--- packages/account/src/Views/emails/pdf-mail.blade.php
+++ packages/account/src/Views/emails/pdf-mail.blade.php
@@ -9,12 +9,12 @@
 <br>
 <div>Please find attached your Account Receivable balance as of {{ $statementDate }}. Your statement total is ${{ $statementTotal }}. If you have any questions regarding your attached statement, please send an email to Employee.AR@adra.org and someone will assist you as soon as possible.</div>
 <br>
-<div><b>NOTE:</b> This is not a spam or phishing email. If you have any concerns please reach out to our IT Department.</div>
+<div><b>NOTE:</b> If you have any concerns this may be a spam or phishing email, please reach out to our IT department.</div>
 <br>
 <div>
 Kind regards,
 <br>
-Nataly Alfaro
+Finance Department
 </div>
 <div style="margin-top:5px;">
     <img src="{{ asset('logos/adra-mail-logo.png') }}" alt="">
